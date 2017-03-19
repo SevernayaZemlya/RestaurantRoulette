25d6b5ed54671b8676f0fe88dba8fee05184ed6d
Chris Dileo <chris.dileo@gmail.com>
2017-03-18 17:28:30 -0700

    Basic top level layout

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Grid, Row, Col } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Grid>
+          <Row className="show-grid">
+            <Col xs={12} md={4}><code>&lt;{'Sidebar Content'} /&gt;</code></Col>
+            <Col xs={12} md={8}><code>&lt;{'Main Content'} /&gt;</code></Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
